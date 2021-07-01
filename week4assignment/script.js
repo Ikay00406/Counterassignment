65053ed757930c2e9eca63ac402e7d4286752407
Ikay00406 <85343341+Ikay00406@users.noreply.github.com>
2021-07-01 12:50:43 +0100

    Add files via upload

---
+++ week4assignment/script.js
@@ -0,0 +1,14 @@
+function decrease(){
+    let a = 1;
+    let textBox = document.getElementById("text");
+    textBox.value--;
+
+}
+function reset(){
+    let textBox = document.getElementById("text");
+    textBox.value = 0;
+}
+function increase(){
+    let textBox = document.getElementById("text");
+    textBox.value++;
+}
